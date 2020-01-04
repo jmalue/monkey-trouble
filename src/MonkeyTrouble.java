62f65c528d7e4b2f482c54b9ff71281056cf071c
jmalue <jrod2416@gmail.com>
2020-01-04 12:57:52 -0500

    monkey-trouble

---
+++ src/MonkeyTrouble.java
@@ -0,0 +1,25 @@
+
+public class MonkeyTrouble {
+
+	public static void main(String[] args) {
+		
+		// We have two monkeys, a and b, 
+		// and the parameters aSmile and bSmile indicate if each is smiling. 
+		// We are in trouble if they are both smiling 
+		// or if neither of them is smiling. 
+		// Return true if we are in trouble.
+		
+		
+		var trouble = monkeyTrouble(true, true);
+		System.out.println(trouble); 
+		}	
+		public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+			if ((aSmile == true && bSmile == true) || (aSmile != true && bSmile !=true))
+			{
+			
+				return true;
+		}
+		return false;
+		}
+}		
+			
